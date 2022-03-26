ffb74e927af2eae378b977ea4d9c2950a111f3ce
fabricedano <dano.fabrice@gmail.com>
2022-03-26 16:19:19 +0100

    Create CurrentAccount and SavingsAccount

---
+++ FDBank/src/components/SavingsAccount.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package components;
+
+/**
+ * @author Fabrice
+ *
+ * 1.2.2 Creation of the CurrentAccount and SavingsAccount
+ */
+public class SavingsAccount extends Account {
+
+	public SavingsAccount(String label, Client client) {
+		super(label, client);
+	}
+
+}
